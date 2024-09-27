d53e5fb0f4b7f7e2344cf417b1c62383ac33388e
Muwonge Hassan Saava <hassansaava@gmail.com>
2024-09-28 00:35:20 +0300

    flyouts

---
+++ ContentPageDemo.xaml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp4;
+
+public partial class ContentPageDemo : ContentPage
+{
+    public ContentPageDemo()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Screens/FlyoutPageDemo.xaml.cs
@@ -0,0 +1,11 @@
+
+
+namespace MauiApp4.Screens;
+
+public partial class FlyoutPage : ContentPage
+{
+    public FlyoutPage()
+    {
+        InitializeComponent();
+    }
+}
